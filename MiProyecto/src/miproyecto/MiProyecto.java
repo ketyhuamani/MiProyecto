ff1cf3074d0da57c8e3a421c5a27956c48ae7e2d
Omar Campos <Omar Campos@192.168.1.100>
2016-06-03 21:28:32 -0500

    version dos de hola lima k

--- MiProyecto/src/miproyecto/MiProyecto.java
+++ MiProyecto/src/miproyecto/MiProyecto.java
@@ -19,7 +19,7 @@ public class MiProyecto {
         System.out.println("Hola Mundo");
         
         System.out.println("Hola Peru");
-        
+        System.out.println("prueba");
         System.out.println("Hola Lima");
     }
     
